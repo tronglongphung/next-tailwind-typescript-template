ce4a34186df2f22b656889c44237967b6c154576
Tom Phung <tronglongphung@gmail.com>
2022-02-18 13:13:27 +1030

    adding container layout

---
+++ components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer className="w-full bg-white border-t border-stone-200 text-stone-800">add footer here</footer>;
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+const navigation = [
+  // { name: 'Giới Thiệu', href: '/about-us' },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function Navbar() {
+  return <nav className="">add navigation here</nav>;
+}
